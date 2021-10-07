098d7a4e8d88ee7fb1ef248112cb70fb9b0ac99b
Jatin Soni <jatinsoni1202@gmail.com>
2021-10-07 08:28:17 +0530

    card section completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import { Header } from "./components/header/Header";
 import { HomeScreen } from "./screens/homeScreen/HomeScreen";
-import { Card } from "./components/card/Card";
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
           <Route path="/">
             <Header />
             <HomeScreen />
-            <Card />
           </Route>
         </Switch>
       </Router>
--- src/components/cardSection/CardSection.js
+++ src/components/cardSection/CardSection.js
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
 import "./cardSection.css";
+import { Card } from "./../card/Card";
 
 export class CardSection extends Component {
   render() {
     return (
       <>
-        <h1>card section</h1>
+        <div className="cardSection">
+          <div className="cardSection_top">
+            <div className="cardSection_title">
+              <h2>Categories</h2>
+            </div>
+            <div className="cardSection_more">
+              <h2>More</h2>
+            </div>
+          </div>
+
+          <div className="cardSection_bottom">
+            <div className="card_holder">
+              <Card />
+            </div>
+            <div className="card_holder">
+              <Card />
+            </div>
+            <div className="card_holder">
+              <Card />
+            </div>
+            <div className="card_holder">
+              <Card />
+            </div>
+          </div>
+        </div>
       </>
     );
   }
