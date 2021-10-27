adb13ee8f63c8c959423f0c6d07a7b8fca7ab22b
Jatin Soni <jatinsoni1202@gmail.com>
2021-10-28 00:22:52 +0530

    detail page styling changed

--- src/Pages/Detail/index.jsx
+++ src/Pages/Detail/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 // importing styles
 import "./style.css";
+import { Link } from "react-router-dom";
 
 const DetailPage = () => {
   return (
@@ -9,8 +10,9 @@ const DetailPage = () => {
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <h1 className="page-title">Linked List</h1>
+            <h1 className="page-title">Sub-Domain</h1>
           </div>
+
           <div className="col-12 col-md-4">
             <div className="poster">
               <img
@@ -19,13 +21,15 @@ const DetailPage = () => {
               />
             </div>
           </div>
+
           <div className="col-12 col-md-8">
             <div className="content">
-              <h2 className="title">
-                Lorem ipsum dolor, sit amet consectetur adipisicing.
-              </h2>
-              <h2 className="date">Lorem ipsum dolor sit amet.</h2>
-              <h2>Lorem, ipsum.</h2>
+              <h2 className="title">This is title of research paper</h2>
+
+              <h2 className="date">Domain</h2>
+
+              <h2>Year of publication - 2018</h2>
+
               <p className="desc">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
                 labore porro et accusamus minus esse exercitationem voluptate
@@ -33,13 +37,14 @@ const DetailPage = () => {
                 natus quae sequi laboriosam rem officia delectus! Minima libero
                 excepturi, veniam voluptas corporis tempore nesciunt facere odio
               </p>
+
               <div className="details-btn">
-                <a href="" className="download-btn">
+                <Link to="" className="download-btn">
                   Download
-                </a>
-                <a href="" className="download-btn">
+                </Link>
+                <Link to="" className="download-btn">
                   Save To Library
-                </a>
+                </Link>
               </div>
             </div>
           </div>
