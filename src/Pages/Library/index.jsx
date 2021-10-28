bb4c9ecbec181d2c34324c6126ed582305e20d71
manish-mehra <manishmehra1469@gmail.com>
2021-10-28 12:00:31 +0530

    add to library page

--- src/Pages/Library/index.jsx
+++ src/Pages/Library/index.jsx
@@ -1,7 +1,90 @@
-import React from "react";
-
+import React from "react"
+import Card from "../../Components/Card"
+import {Link} from 'react-router-dom'
+import "./style.css"
 const Library = () => {
-  return <div>Library Page</div>;
+  return (
+    <div className="library-page">
+
+      <div className="header">
+        <h1>Library</h1>
+      </div>
+
+      <div className="main">
+
+        <div className="category">                  
+          <div className="category-title">
+            <h2>Object Oriented Programming</h2>
+          </div>
+          <div className="category-items">           
+              <div className="col-lg-4 col-md-6 col-12 ">
+                <Link to="/detail">
+                  <Card />
+                </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="category">                  
+          <div className="category-title">
+            <h2>Physics</h2>
+          </div>
+          <div className="category-items">           
+              <div className="col-lg-4 col-md-6 col-12 ">
+                <Link to="/detail">
+                  <Card />
+                </Link>
+              </div>
+
+              <div className="col-lg-4 col-md-6 col-12 ">
+                <Link to="/detail">
+                  <Card />
+                </Link>
+              </div>
+
+              <div className="col-lg-4 col-md-6 col-12 ">
+                <Link to="/detail">
+                  <Card />
+                </Link>
+              </div>
+          </div>
+        </div>
+
+        <div className="category">                  
+          <div className="category-title">
+            <h2>Mathematics</h2>
+          </div>
+          <div className="category-items">           
+              <div className="col-lg-4 col-md-6 col-12 ">
+                <Link to="/detail">
+                  <Card />
+                </Link>
+              </div>
+
+              <div className="col-lg-4 col-md-6 col-12 ">
+                <Link to="/detail">
+                  <Card />
+                </Link>
+              </div>
+
+              <div className="col-lg-4 col-md-6 col-12 ">
+                <Link to="/detail">
+                  <Card />
+                </Link>
+              </div>
+
+              <div className="col-lg-4 col-md-6 col-12 ">
+                <Link to="/detail">
+                  <Card />
+                </Link>
+              </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  )
 };
 
 export default Library;
