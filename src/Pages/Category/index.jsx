3fa0dfcbdcd89c73c2862799c8a4e00c9c2f0e4c
Jatin Soni <jatinsoni1202@gmail.com>
2021-11-01 12:07:34 +0530

    category page text edited

--- src/Pages/Category/index.jsx
+++ src/Pages/Category/index.jsx
@@ -1,43 +1,41 @@
 import React from "react";
-import Card from "../../Components/Card"
-import {Link} from 'react-router-dom'
-import './style.css'
+import Card from "../../Components/Card";
+import { Link } from "react-router-dom";
+import "./style.css";
 const Category = () => {
-  const subDomains = ['Sub-Domain', 'Sub-Domain']
+  const subDomains = ["Sub-Domain", "Sub-Domain"];
   return (
     <div className="category-page">
-       <div className="header">
-        <h1>Physics</h1>
+      <div className="header">
+        <h1>Domain</h1>
       </div>
-    
-    <div className="main">
-        
-    {subDomains.map((subdomain)=>{
-      return <section className="subdomains">
 
-          <div className="subdomain-header">
-            <h2>{subdomain}</h2>
-          </div>
-          <div className="subdomain-items">
-            <div className="col-lg-4 col-md-6 col-12 ">
+      <div className="main">
+        {subDomains.map((subdomain) => {
+          return (
+            <section className="subdomains">
+              <div className="subdomain-header">
+                <h2>{subdomain}</h2>
+              </div>
+              <div className="subdomain-items">
+                <div className="col-lg-4 col-md-6 col-12 ">
                   <Link to="/detail">
                     <Card />
                   </Link>
-              </div>
+                </div>
 
-              <div className="col-lg-4 col-md-6 col-12 ">
-                <Link to="/detail">
-                  <Card />
-                </Link>
+                <div className="col-lg-4 col-md-6 col-12 ">
+                  <Link to="/detail">
+                    <Card />
+                  </Link>
+                </div>
               </div>
-          </div>
-      
-      </section>
-    })}
-
+            </section>
+          );
+        })}
+      </div>
     </div>
-    </div>
-  )
+  );
 };
 
 export default Category;
