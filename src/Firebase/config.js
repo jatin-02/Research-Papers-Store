3b2881f2ae33a465a1873e364256bc04c3bdd642
Sahil Verma <sahilvermaweb.dev@gmail.com>
2021-11-04 13:55:24 +0000

    Make sinle category pages with firebase aand unnecessary code and files

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -1,18 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-import { getFirestore } from "firebase/firestore/lite";
+import { getFirestore } from "firebase/firestore/";
 import { getAuth } from "firebase/auth";
 
-const {FIREBASE_API_KEY, FIREBASE_API_ID} = process.env
+const { FIREBASE_API_KEY, FIREBASE_API_ID } = process.env;
 const firebaseConfig = {
-  apiKey: `"${FIREBASE_API_KEY}`,
+  apiKey: "AIzaSyAO0OE2PefvPhh1ccBdOT71_cpw8V96dxs",
   authDomain: "reseachit.firebaseapp.com",
   projectId: "reseachit",
   storageBucket: "reseachit.appspot.com",
   messagingSenderId: "1055291125060",
-  appId: `${FIREBASE_API_ID}`,
+  appId: "1:1055291125060:web:df1f4da58fb6c59decfb11",
 };
+
+console.log(FIREBASE_API_KEY, FIREBASE_API_ID);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
